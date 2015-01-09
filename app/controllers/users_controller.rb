5369fe5225d4c900666b2fa15efbb486c5a4552f
Joshua Munene <joshuamwangi@gmail.com>
2015-01-09 12:53:36 -0800

    filled in some stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+	def index
+		@users = User.all 
+	end
+
+	def show
+		@user = User.find(params [:id])
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new
+		if @user.save
+			redirect_to list_path
+		else
+			render :new
+		end
+
+	def edit
+
+	end
+
+	def destroy
+		@user = User.destroy
+	end
+
+	private
+		def user_params
+			params.require(:user).permit(:image, :email, :first_name, :last_name, :password)
+		end
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User
   field :first_name, type: String
   field :last_name, type: String
   field :email, type: String
+
+	#mount_uploader :image, AvatarUploader
+
+  # def password=(unencrypted_password)
+  # 	self.password_digest = BCrypt::Password.create(unencrypted_password)
+  # end
+
+  # def authenticate(unencrypted_password)
+  # 	if BCrypt::Password.new(self.password_digest) == unencrypted_password
+  # 		return self
+  # 	else
+  # 		return false
+  # 	end
+  # end
+
 end
