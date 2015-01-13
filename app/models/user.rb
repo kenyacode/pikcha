c769eb3d4703b4f35ee278620a4587629e41d4ec
Joshua Munene <joshuamwangi@gmail.com>
2015-01-13 11:11:35 -0800

    added authentication into User.rb, added a route for /signup => users#new.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,20 +3,27 @@ class User
   field :first_name, type: String
   field :last_name, type: String
   field :email, type: String
+  attr_reader :password
 
 	mount_uploader :image, AvatarUploader
   field :remove_image
 
-  # def password=(unencrypted_password)
-  # 	self.password_digest = BCrypt::Password.create(unencrypted_password)
-  # end
+   def password=(unencrypted_password)
+    unless unencrypted_password.empty?
+      # keep the unencrypted password in memory for a second
+      @password = unencrypted_password
+      # use BCrypt to encrypt our plain password into a digest
+      # and store it in our database
+     	self.password_digest = BCrypt::Password.create(unencrypted_password)
+      end
+    end
 
-  # def authenticate(unencrypted_password)
-  # 	if BCrypt::Password.new(self.password_digest) == unencrypted_password
-  # 		return self
-  # 	else
-  # 		return false
-  # 	end
-  # end
+   def authenticate(unencrypted_password)
+   	if BCrypt::Password.new(self.password_digest) == unencrypted_password
+   		return self
+   	else
+   		return false
+   	end
+   end
 
 end
