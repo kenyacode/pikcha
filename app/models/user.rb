7e42ec8834710a2c9da60fb81aba2df2e4d863a8
Joshua Munene <joshuamwangi@gmail.com>
2015-01-14 06:03:44 -0800

    added validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,11 @@ class User
 	mount_uploader :image, AvatarUploader
   field :remove_image
 
+validates :first_name, presence: true
+validates :last_name, presence: true
+validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+validates :password, presence: true, confirmation: true, length: { in: 6..20 }
+
    def password=(unencrypted_password)
     unless unencrypted_password.empty?
       # keep the unencrypted password in memory for a second
