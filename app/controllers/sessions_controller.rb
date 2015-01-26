3d4a3ed1eee18803ec28b1f1424875704bf7a7f4
Joshua Munene <joshuamwangi@gmail.com>
2015-01-26 00:03:43 -0800

    modified nav-bar to show user namewhen logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
 		user = User.where(email: params[:login][:email]).first
 		if user && user.authenticate(params[:login][:password])
 			session[:user_id] = user.id.to_s
+			session[:user_name] = user.first_name + " " + user.last_name
 			redirect_to user
 		else
 			flash[:fail] = "Your log in failed"
